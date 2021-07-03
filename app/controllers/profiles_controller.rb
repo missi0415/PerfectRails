66fef122a5d845f81b314dda9d5e9ea59a81e584
missi0415 <missi0415@gmail.com>
2021-07-03 12:05:51 +0900

    明示的にstrong_parameterを変える

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,25 @@
+class ProfilesController < ApplicationController
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    user = current_user
+    # params[:user] => {name: "bob",email: "bob@example.com"}
+    user.update(user_params)
+  end
+
+  private
+  #外部から渡されるパラメータをそのまま信用せず
+  #明示的に許可したキーのみを利用する。
+
+  def user_params
+    if current_user.admin?
+      params.require(:user).permit(:name,:email,:admin)
+    else
+      params.require(:user).permit(:name,:email)
+    end
+  end
+end
